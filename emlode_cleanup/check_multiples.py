63703935c27d1d7be436816e72b372f89ab57922
diaksz <diaks.z@husky.neu.edu>
2018-11-04 23:02:21 -0500

    updating emlode script to work over all datasets

--- emlode_cleanup/check_multiples.py
+++ emlode_cleanup/check_multiples.py
@@ -6,7 +6,7 @@ import sys
 from matplotlib.widgets import Button
 
 # Setup file stuff
-cd = '/mnt/emlode16_tank2/'
+cd = os.getcwd()
 files = os.listdir(cd)
 lst = []
 lst2 = []
@@ -16,34 +16,34 @@ plt.gray()
 plt.close()
 # Set up figures with buttons
 
-class callbacks():
-
-    def plot_1(self,event):
-        print 'Keeping image'
-	plt.close()
-    def plot_2(self,event):
-	os.rename(self.fname,cd+'cb2_bad_acq/%s'%self.fname)
-	print '%s moved to cb2_bad_acq'%self.fname	
-	plt.close()
-    def __init__(self,im,fname):
-        self.fname = fname
-	self.im = im
-        plt.figure()
-	plt.imshow(im)
-        self.ax = plt.subplot()
-	plt.subplots_adjust(bottom=0.2)
-	plt.title(fname,pad=10)
-        self.ax1 = plt.axes([0.7, 0.05, 0.1, 0.075])
-        self.ax2 = plt.axes([0.81, 0.05, 0.1, 0.075])
-        self.ba = Button(self.ax1,'Keep')
-        self.bb = Button(self.ax2,'Bad Acq')
-        self.ba.on_clicked(self.plot_1)
-        self.bb.on_clicked(self.plot_2)
-
 if __name__ == "__main__":
-	start = sys.argv[1]
-	end = sys.argv[2]
-	debug = sys.argv[3]
+	dataset = sys.argv[1] 
+        start = sys.argv[2]
+	end = sys.argv[3]
+	class callbacks():
+
+            def plot_1(self,event):
+                print 'Keeping image'
+                plt.close()
+            def plot_2(self,event):
+                os.rename(self.fname,cd+'%s_bad_acq/%s'%(dataset,self.fname))
+                print '%s moved to bad_acq'%self.fname	
+                plt.close()
+            def __init__(self,im,fname):
+                self.fname = fname
+                self.im = im
+                plt.figure()
+                plt.imshow(im)
+                self.ax = plt.subplot()
+                plt.subplots_adjust(bottom=0.2)
+                plt.title(fname,pad=10)
+                self.ax1 = plt.axes([0.7, 0.05, 0.1, 0.075])
+                self.ax2 = plt.axes([0.81, 0.05, 0.1, 0.075])
+                self.ba = Button(self.ax1,'Keep')
+                self.bb = Button(self.ax2,'Bad Acq')
+                self.ba.on_clicked(self.plot_1)
+                self.bb.on_clicked(self.plot_2)
+
 
 	for i in files:
 		if not i[-1].isdigit():
@@ -58,7 +58,7 @@ if __name__ == "__main__":
 		print lst	
 		for montage in lst:
 			if not os.path.exists('%s/%s_coarse_montage.tif'%(montage,montage)):
-				os.rename(montage,cd + 'cb2_trash/%s'%montage)
+				os.rename(montage,cd + '%s_trash/%s'%(dataset,montage)
 				print '%s moved to trash'%montage			
 			else:
 				print montage
